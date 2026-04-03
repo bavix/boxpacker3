ee3315f9995dbbdcbfcd671deb99e015fd2c137c
Maxim Babichev <maksim.babichev95@gmail.com>
2026-04-03 20:16:25 +0300

    add strategy name tests

---
+++ strategy_names_shared_test.go
@@ -0,0 +1,17 @@
+package boxpacker3_test
+
+const (
+	nameFewestBoxes              = "FewestBoxes"
+	nameLeastVolume              = "LeastVolume"
+	nameMostItems                = "MostItems"
+	nameHighestFill              = "HighestFill"
+	nameFirstFitDecreasing       = "FirstFitDecreasing"
+	nameFirstFitIncreasing       = "FirstFitIncreasing"
+	nameBestFitIncreasing        = "BestFitIncreasing"
+	nameBestFitDecreasing        = "BestFitDecreasing"
+	nameNextFitIncreasing        = "NextFitIncreasing"
+	nameWorstFitIncreasing       = "WorstFitIncreasing"
+	nameAlmostWorstFitIncreasing = "AlmostWorstFitIncreasing"
+	nameFullestBoxDecreasing     = "FullestBoxDecreasing"
+	nameBalancedWeight           = "BalancedWeight"
+)
